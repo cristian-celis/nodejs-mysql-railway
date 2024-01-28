71908a74fee870d333931eb26cf512b291fee168
Cristian Celis <cristian.celis02@uptc.edu.co>
2024-01-28 18:27:42 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,25 @@
+import {createPool} from 'mysql2/promise'
+import {
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_PORT,
+    DB_USER
+} from './config.js'
+
+export const pool = createPool({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME
+})
+
+/*export const pool = createPool({
+    user: 'root',
+    password: 'jcelis',
+    host: 'localhost',
+    port: 3306,
+    database: 'usersdb'
+})*/
+
